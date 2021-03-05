618fdbcced5b6e2567f4dcdb9377dfc9cc555526
Fionnie Pollack <fionniepollack@gmail.com>
2021-03-05 05:32:54 +0000

    Add instruction buttons to create recipe form

--- static/js/create-recipe.js
+++ static/js/create-recipe.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//----------------------------------------------------------------------------//
+//- INGREDIENT BUTTONS -------------------------------------------------------//
+//----------------------------------------------------------------------------//
 
 // Button to add an ingredient
 const addIngredientButton = $('#add-ingredient');
@@ -25,5 +28,42 @@ function removeIngredient() {
     if (ingredientInputListLength > 1) {
     $('#ingredient-item-' + ingredientInputListLength).remove();
     $('#num-ingredient').val(ingredientInputListLength - 1);
+    }
+}
+
+
+//----------------------------------------------------------------------------//
+//- INSTRUCTION BUTTONS ------------------------------------------------------//
+//----------------------------------------------------------------------------//
+
+// Button to add an instruction
+const addInstructionButton = $('#add-instruction');
+addInstructionButton.on('click', addInstruction);
+
+function addInstruction() {
+  console.log("Running addInstruction..."); 
+  const newInstructionNum = parseInt($('#num-instruction').val()) + 1;
+
+  console.log("newInstructionNum = " + newInstructionNum)
+
+  const newInstructionInput = "<input type='text' id='instruction-item-" + newInstructionNum + "'><br>";
+
+  console.log("Appending newInstructionInput: " + newInstructionInput)
+  $('#instruction-input-list').append(newInstructionInput);
+
+  $('#num-instruction').val(newInstructionNum);
+}
+
+
+// Button to remove an instruction
+const removeInstructionButton = $('#remove-instruction');
+removeInstructionButton.on('click', removeInstruction);
+
+function removeInstruction() {
+    const instructionInputListLength = $('#num-instruction').val();
+
+    if (instructionInputListLength > 1) {
+    $('#instruction-item-' + instructionInputListLength).remove();
+    $('#num-instruction').val(instructionInputListLength - 1);
     }
 }
